65a444c2d7084872bb09242072b0ea9fbcc01d32
NikhilRaghavan45 <135696240+NikhilRaghavan45@users.noreply.github.com>
2023-11-15 18:41:42 +0530

    Add files via upload
    15-11-23

---
+++ src/components/AboutComp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import '../components/About.css'
+
+
+class AboutComp extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default AboutComp
---
+++ src/components/MenuComp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class MenuComp extends Component {
+    render() {
+        return (
+            <div>
+                <h2>hiii</h2>
+                
+            </div>
+        )
+    }
+}
+
+export default MenuComp
